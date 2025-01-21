4df02999eea647159c1236e1312a3f0a402a8722
AmbujKRai <ambuj.rai06@gmail.com>
2025-01-22 03:38:23 +0530

    setup

---
+++ online_compiler/src/App.jsx
@@ -0,0 +1,12 @@
+import Input from "./components/input.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Input />
+    </>
+  )
+}
+
+export default App
---
+++ online_compiler/src/components/input.jsx
@@ -0,0 +1,20 @@
+function Input() {
+  return (
+    <>
+
+    <h1>Bootstrap</h1>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">
+          Example textarea
+        </label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+    </>
+  );
+}
+
+export default Input;
