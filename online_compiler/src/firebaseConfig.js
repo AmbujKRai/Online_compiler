b6f9c0763a1770175697db8a0851fbb138c6f30f
AmbujKRai <145692057+AmbujKRai@users.noreply.github.com>
2025-04-22 20:37:34 +0530

    Update firebaseConfig.js

--- online_compiler/src/firebaseConfig.js
+++ online_compiler/src/firebaseConfig.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAzECo-3dANCy1N76LSKClerhe1DpIbsgE",
   authDomain: "online-compiler-c53c4.firebaseapp.com",
@@ -14,6 +12,5 @@ const firebaseConfig = {
   appId: "1:607340835812:web:84e4e2c9baa367bff70df1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
